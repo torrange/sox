50f3b219e656aa9b019debd1b7bab57074d6a5c6
torrange <tobiasjone@hush.com>
2014-01-26 04:06:11 +0000

    Update sox_batch.py

--- sox_batch.py
+++ sox_batch.py
@@ -8,9 +8,7 @@ from datetime import date, datetime
 
 
 class Sox(object):
-
 	def __init__(self, samplerate="44100"):
-
 		self.today = date.strftime(datetime.now(), "%Y%m%d")
 		
 		try:
@@ -21,54 +19,37 @@ class Sox(object):
 			self.samplerate = samplerate
 
 		self.filetypes = ("wav", "aif", "aiff") 
-		
 		self.cwd = getcwd()
 
 
-
-
 	def inputFiles(self):
-
 		self.input_files=[]
 
 		for filetype in self.filetypes:
-		    
 		    intemp=glob(self.cwd+"/*.%s" % (filetype))
-		    
+		   
 		    if intemp >=1:
 			
 			for filename in intemp:
-			    
 			    self.input_files.append(filename)
 
 
-
-
 	def convertFiles(self):
-
 		if len(self.input_files) >=1:
 		    
 		    global converted_count	
-		    
 		    converted_count = 0
 		    
-		    
 		    try:
-			
 			mkdir(self.cwd+"/converted")
 		    
-		    
 		    except OSError:
-			
 			exit("'converted' directory already exists")
 		
-		    
 		    for input_file in self.input_files:
-			
 			destination_samplerate = self.samplerate
 
 			_rawfilename=input_file.split("/")
-			
 			self.rawfilename =_rawfilename[len(_rawfilename)-1]
 
 			destination_filename = "%s_%s__%s" % (destination_samplerate[0:2], self.today, self.rawfilename )
@@ -78,21 +59,14 @@ class Sox(object):
 			converted_count += 1  
 		    		    
 		else:
-		    
 		    exit("No files to process")
 
 
 
-
-
 if __name__ == "__main__":
-	
 	s=Sox() 
-	
 	s.inputFiles()
-	
 	s.convertFiles()
-	
 	exit("Converted %d files" % converted_count)
 
 
